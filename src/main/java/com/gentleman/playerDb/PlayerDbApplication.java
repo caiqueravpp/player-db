1f7a0266e843da992b74c86d5053870495b461ff
Caiquera <caiquerantc@gmail.com>
2020-07-02 16:35:30 -0300

    Primeiro commit

---
+++ src/main/java/com/gentleman/playerDb/PlayerDbApplication.java
@@ -0,0 +1,13 @@
+package com.gentleman.playerDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerDbApplication.class, args);
+	}
+
+}
